b6bdfe8bf8c577858197fd951cfafe7e04d28fed
Aitor Corrales Rodriguez <metalocalypsis@gmail.com>
2015-09-03 13:01:17 +0200

    project

---
+++ manaject/src/essentials/Project.java
@@ -0,0 +1,14 @@
+package essentials;
+
+import java.util.Vector;
+
+public class Project {
+	
+	private String name;
+	private Vector <Competence> comp;
+	
+	public Project(String name, Vector <Competence> comp){
+		
+	}
+
+}
